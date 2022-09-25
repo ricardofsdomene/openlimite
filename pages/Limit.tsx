8ebb9119e9a5df76a0718657278510a42383d11a
ricardofsdomene <ricardofsdomene@icloud.com>
2022-09-25 18:27:01 -0300

    chore: changeLimit context

--- pages/Limit.tsx
+++ pages/Limit.tsx
@@ -454,7 +454,7 @@ export default function LimitScreen({}) {
           onPress={() => {
             navigation.navigate("#BTGFazTech - OpenLimite", {});
             changeLimit({
-              amount: selectedLimit,
+              amount: selectedLimit > 0 ? selectedLimit : cardLimit.limitAmount,
             });
           }}
         />
